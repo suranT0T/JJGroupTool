6e7e8d7eff9dbfd492bd937bd28dfa6508432263
suran <suran2014@outlook.com>
2021-03-13 21:38:40 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace 静静分组小工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(3, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(139, 338);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(229, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(254, 338);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "分组";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(160, 31);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(38, 21);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 362);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "静静分组小工具";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 静静分组小工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        class record
+        {
+            public string num { get; set; }
+            public float value { get; set; }
+            public bool emptySign = false;
+        }
+
+        List<record> records = new List<record> { };
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                records = new List<record> { };
+                //清空并重新排序展示
+                string text = textBox1.Text;
+                string[] textarr = text.Split('\n');
+                foreach (var item in textarr)
+                {
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        string buf = "";
+                        List<string> arBuf = new List<string> { };
+                        foreach (var cr in item)
+                        {
+                            if (cr > 32)
+                                buf += cr;
+                            else
+                            {
+                                if (!string.IsNullOrEmpty(buf))
+                                {
+                                    arBuf.Add(buf);
+                                    buf = "";
+                                }
+                            }
+                        }
+                        arBuf.Add(buf);
+                        if (arBuf.Count > 1)
+                        {
+                            records.Add(new record { num = arBuf[0], value = float.Parse(arBuf[1]) });
+                        }
+                           
+                    }
+                }
+                text = "";
+                records = records.OrderBy(r => r.value).ToList();
+                foreach (var item in records)
+                {
+                    text += (item.num + "  " + item.value + "\r\n");
+                }
+
+                this.textBox1.TextChanged -= new EventHandler( textBox1_TextChanged);
+                this.textBox1.Text = text;
+                this.textBox1.TextChanged += new EventHandler(textBox1_TextChanged);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("数据格式错误，检查后重试");
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = "";
+            int gcount = (int)this.numericUpDown1.Value;
+            Dictionary<string, List<record>> result = new Dictionary<string, List<record>> { };
+            List<record> buf = new List<record> { };
+            for (int i = 0; i < records.Count; i++)
+            {
+                buf.Add(records[i]);
+                if ((i+1) % gcount == 0)
+                    buf.Add(new record { emptySign = true });
+            }
+            //分组
+            for (int i = 0; i < buf.Count; i++)
+            {
+                string gname = "G" + ((i % gcount)+1).ToString();
+                if (!result.ContainsKey(gname))
+                    result[gname] = new List<record> { };
+                if(buf[i].emptySign!=true)
+                    result[gname].Add(buf[i]);
+            }
+            //显示
+            //foreach (var item in result)
+            //{
+            //    text += item.Key + "\r\n";
+            //    foreach (var va in item.Value)
+            //    {
+            //        text += (va.num + '\t' + va.value+"\r\n");
+            //    }
+            //}
+            //textBox2.Text = text;
+            int linecount = 0;
+            int nodecount = records.Count;
+            while (nodecount>0)
+            {
+                foreach (var item in result)
+                {
+                    if (linecount == 0)
+                    {
+                        text += (item.Key + "\t\t");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            text += (item.Value[linecount-1].num + '\t' + item.Value[linecount-1].value + '\t');
+                            nodecount--;
+                        }
+                        catch (Exception)
+                        {
+
+                           
+                        }
+                        
+                    }
+                }
+                linecount++;
+                text += "\r\n";
+            }
+            textBox2.Focus();
+            textBox2.Text = text;
+            textBox2.SelectAll();
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
